90cff8acf7ffa2f5fbfb3d766ac07bf388ed855f
André Murakami <drehkenzo20@gmail.com>
2020-02-16 22:27:33 -0300

    criando rotas e instalando express

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const http = require('http');
 
+http.createServer((req,res)=>{
+  res.writeHead(200, {"content-type": "text/plain"});
+  
+  switch(req.url){
+    case "/":
+      res.end("Voce esta na pagina home");
+      break;
+    case "/contato":
+      res.end("Voce esta na pagina de contato");
+      break;
+    default:
+      res.end("Voce esta em nosso servidor");
+  }
+  req.url
+
+  res.end("Meu primeiro servidor");
+
+}).listen(3000, 'localhost');
